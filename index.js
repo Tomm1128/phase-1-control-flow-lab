59c55397d3b6a4afc8f3d9abeac1a88a1d9375c2
Thomas Martin <thomas.j.martin@powerhrg.com>
2024-05-06 11:55:45 -0400

    Clean up conditional for scuberGreetingForFeet function

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function scuberGreetingForFeet(distance){
 
   if (distance <= 400){
     return "This one is on me!";
-  } else if (distance > 400 && distance <= 2000){
+  } else if (distance <= 2000){
     return "That will be twenty bucks.";
   } else if (distance > 2000 && distance <= 2500){
     return "I will gladly take your thirty bucks."
